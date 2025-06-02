ef725ec8bbba165a52e68243c5a38d9ba8ae5069
João Paiva <joaopaiva.oliveiraneto@gmail.com>
2025-06-02 17:03:37 -0300

    First commit, Introdução do Curso

---
+++ src/inf/aula5.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var nome string = "Joao"
+	var idade int = 28
+	var versao float32 = 3.2
+	fmt.Println("Meu nome eh", nome, "e minha idade eh", idade)
+	fmt.Println("Este programa esta na versao", versao)
+
+}
